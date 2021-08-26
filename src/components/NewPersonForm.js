2a5aec46d03c35b88dcb5159d29703721903d02a
David Stiennon <david.stiennon@gmail.com>
2021-08-26 13:21:52 -0500

    styling on new person form

--- src/components/NewPersonForm.js
+++ src/components/NewPersonForm.js
@@ -1,11 +1,33 @@
   import React, {useState} from 'react';
 import {Input, Button, TextField} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ( {
+  formLabel: {
+    color: 'green'
+  },
+  formSpacing: {
+    display: 'flex',
+    width: '50%',
+    flexDirection: 'column',
+    '& $formEntry': {
+      margin: 20
+    }
+  },
+  formEntry: {
+    display: 'flex',
+    width: 300,
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  }
+} ))
 
 const NewPersonForm = (props) => {
   const [name, setName] = useState("David")
   const [address, setAddress] = useState("123 Woodland")
   const [email, setEmail] = useState('dstiennon@lenderclose.com')
   
+  const styles = useStyles()
   
   const handleNameChange = function(e) {
     setName(e.target.value)
@@ -31,20 +53,20 @@ const NewPersonForm = (props) => {
   }
   
   return (
-    <form>
-      <div>
-        <label>Name</label>
+    <form className={styles.formSpacing}>
+      <div className={styles.formEntry}>
+        <label className={styles.formLabel}>Name</label>
         <TextField variant="outlined" color="primary" label="name" value={name} onChange={handleNameChange}/>
       </div>
-      <div>
-        <label>Address</label>
+      <div className={styles.formEntry}>
+        <label className={styles.formLabel}>Address</label>
         <TextField variant="outlined" color="primary" value={address} onChange={handleAddressChange}/>
       </div>
-      <div>
-        <label>Email</label>
+      <div className={styles.formEntry}>
+        <label className={styles.formLabel}>Email</label>
         <TextField variant="outlined" color="primary" value={email} onChange={handleEmailChange}/>
       </div>
-      <div>
+      <div className={styles.formEntry}>
         <Button variant="contained" color="primary" onClick={handleButtonClick} >Add</Button>
       </div>
     </form>
